c8494ee405b2bcebf66ef6c006dedb2426ad92ea
timmson <timmson666@mail.ru>
2017-01-18 22:13:21 +0300

    Add task to dml

--- db/02-dml.sql
+++ db/02-dml.sql
@@ -1,4 +1,7 @@
 USE `fruit`;
 
 -- projects
-INSERT INTO `fm_project` VALUES (1, 'SAMPLE', 'Some new project',  'manager', null);
+INSERT INTO `fm_project` VALUES (1, 'SAMPLE', 'Some new project', 'manager', NULL);
+
+-- Tasks
+INSERT INTO `fm_task` VALUES (1, 'Test', 'Test', 1, 1, 2, 100, 'fruit');
