733230a8f7503a4ff1c4d1ef774fe90a288b1c77
timmson <timmson666@mail.ru>
2020-10-05 16:19:53 +0300

    Add first test

---
+++ test/CalendarTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ru\timmson\FruitMamangement\util;
+
+use DateTime;
+use \PHPUnit\Framework\TestCase;
+
+require_once (__DIR__."/../src/lib/calendar.inc.php");
+
+class CalendarTest extends TestCase
+{
+    public function testGetTaskEndInTheCurrentDay()
+    {
+        $start = new DateTime("2020-09-23T09:00");
+
+        $result = getTaskEnd($start->getTimestamp(), 1);
+        $resultInDateTime = new DateTime("@$result");
+
+        $this->assertEquals(new DateTime("2020-09-23T10:00"), $resultInDateTime);
+    }
+
+    public function testGetTaskEndInTheEndIfTheCurrentDay()
+    {
+        $start = new DateTime("2020-09-23T09:00");
+
+        $result = getTaskEnd($start->getTimestamp(), 7);
+        $resultInDateTime = new DateTime("@$result");
+
+        $this->assertEquals(new DateTime("2020-09-23T16:00"), $resultInDateTime);
+    }
+
+    public function testGetTaskEndInTheNextDay()
+    {
+        $start = new DateTime("2020-09-23T09:00");
+
+        $result = getTaskEnd($start->getTimestamp(), 8);
+        $resultInDateTime = new DateTime("@$result");
+
+        $this->assertEquals(new DateTime("2020-09-24T09:00"), $resultInDateTime);
+    }
+
+    public function testGetTaskEndInTheNextWeek()
+    {
+        $start = new DateTime("2020-09-23T09:00");
+
+        $result = getTaskEnd($start->getTimestamp(), 24);
+        $resultInDateTime = new DateTime("@$result");
+
+        $this->assertEquals(new DateTime("2020-09-28T09:00"), $resultInDateTime);
+    }
+
+}
