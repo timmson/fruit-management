57ec39b3ffe38f0a23f3abddbe47e63d7c3f0fc3
timmson <timmson666@mail.ru>
2023-05-14 20:30:31 +0300

    Add User

--- front/src/user/user.tsx
+++ front/src/user/user.tsx
@@ -1,7 +1,25 @@
 import React from "react"
+import HomeTask from "../home/home-task"
+import HomeActivity from "../home/home-activity"
 
 export function User() {
 	return (
-		<h1>Hello!</h1>
+		<>
+			<div className="row">
+				<div className="col">
+					<h2>Пользователь</h2>
+				</div>
+			</div>
+			<div className="row mt-2">
+				<div className="col">
+					<HomeTask tasks={[]}/>
+				</div>
+			</div>
+			<div className="row mt-2">
+				<div className="col">
+					<HomeActivity activities={[]}/>
+				</div>
+			</div>
+		</>
 	)
 }
